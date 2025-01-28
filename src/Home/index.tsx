e4eb7b588d6905219177c0cbbc005091d23a080c
Gabriele Carvalho <gabi.05assis9@gmail.com>
2025-01-28 15:09:21 -0300

    ajuste

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -33,7 +33,7 @@ function Home() {
       title: 'Gestão de Pedidos',
       items: [
         { name: 'Rastreamento de Pedidos', link: '/rastreamento-pedidos' },
-        { name: 'Solicitação de Entregas', link: '/GestaoPedidos' },
+        { name: 'Solicitação de Entregas', link: '/FormularioFrete' },
       ],
     },
     {
